ab9c48ded015605fe6d3c55f2783a610fa8beacf
Frank Nägler <fn@iosoft.de>
2013-10-11 15:35:54 +0200

    Revert "[FEATURE] add the possiblity to set a active CSS class for menu items."
    This reverts commit d19d11a60f684a21b3ed22d0c9e243d0e75d476b.

--- at_navigation.php
+++ at_navigation.php
@@ -81,13 +81,12 @@ class AT_Navigation {
 			$child = $start ? sprintf($ul, $id, $class, $child) : sprintf($ul, $child);
 		}
 		
-		$navTitle	= (isset($navigation['navtitle'])) ? $navigation['navtitle'] : $navigation['title'];
 		$li = isset($navigation['title'])
 			? sprintf(
 				'<li class="%1$s"><a href="%2$s" class="%1$s" title="%3$s">%3$s</a>%4$s</li>',
 				$navigation['class'],
 				$navigation['url'],
-				$navTitle,
+				$navigation['title'],
 				$child
 			)
 			: $child;
@@ -128,14 +127,13 @@ class AT_Navigation {
 	
 	private function at_recursive($split = array(), $page = array(), $current_page = array())
 	{
-		$activeClass = (isset($this->settings['at_navigation']['activeClass'])) ? $this->settings['at_navigation']['activeClass'] : 'is-active';
 		if (count($split) == 1)
 		{			
 			$is_index = ($split[0] == '') ? true : false;
 			$ret = array(
-				'title'			=> (isset($page['navtitle']) ? $page['navtitle'] : $page['title']),
+				'title'			=> $page['title'],
 				'url'			=> $page['url'],
-				'class'			=> ($page['url'] == $current_page['url']) ? $activeClass : ''
+				'class'			=> ($page['url'] == $current_page['url']) ? 'is-active' : ''
 			);
 			
 			$split0 = ($split[0] == '') ? '_index' : $split[0];
